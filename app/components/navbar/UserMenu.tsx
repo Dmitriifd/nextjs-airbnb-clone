84b69319377b1228a3849cb8893f0f08705914d8
Dmitriifd <idimka9@gmail.com>
2023-11-17 19:28:37 +0500

    added types

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -9,10 +9,10 @@ import useRegisterModal from '@/app/hooks/useRegisterModal';
 import useLoginModal from '@/app/hooks/useLoginModal';
 import MenuItem from './MenuItem';
 import Avatar from '../Avatar';
-import { User } from '@prisma/client';
+import { SafeUser } from '@/app/types';
 
 interface UserMenuProps {
-  currentUser?: User | null
+  currentUser?: SafeUser | null
 }
 
 const UserMenu: React.FC<UserMenuProps> = ({
