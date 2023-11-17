e854f30f7fe59ba81851abe63f897b2e0ffed54e
Dmitriifd <idimka9@gmail.com>
2023-11-17 22:07:25 +0500

    RentModal Step location

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import dynamic from 'next/dynamic';
 import { useRouter } from 'next/navigation';
 import { useMemo, useState } from 'react';
 
@@ -8,6 +9,7 @@ import useRentModal from '@/app/hooks/useRentModal';
 
 import Modal from './Modal';
 import CategoryInput from '../inputs/CategoryInput';
+import CountrySelect from '../inputs/CountrySelect';
 import { categories } from '../navbar/Categories';
 import Heading from '../Heading';
 
@@ -48,7 +50,16 @@ const RentModal = () => {
     },
   });
 
+  const location = watch('location');
   const category = watch('category');
+
+  const Map = useMemo(
+    () =>
+      dynamic(() => import('../Map'), {
+        ssr: false,
+      }),
+    [location]
+  );
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -115,6 +126,22 @@ const RentModal = () => {
       </div>
     </div>
   );
+
+  if (step === STEPS.LOCATION) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading
+          title='Where is your place located?'
+          subtitle='Help guests find you!'
+        />
+        <CountrySelect
+          value={location}
+          onChange={(value) => setCustomValue('location', value)}
+        />
+        <Map center={location?.latlng} />
+      </div>
+    );
+  }
 
   return (
     <Modal
