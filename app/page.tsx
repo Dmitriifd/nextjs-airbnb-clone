626f77544760f309eeefc7250334c55aa55b88f2
Dmitriifd <idimka9@gmail.com>
2023-11-18 14:28:55 +0500

    Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,40 @@
-export default function Home() {
-  return <main>Home page</main>;
-}
+import Container from '@/app/components/Container';
+import EmptyState from '@/app/components/EmptyState';
+
+import ClientOnly from './components/ClientOnly';
+
+const Home = async () => {
+  const isEmpty = true
+
+  if (isEmpty) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <Container>
+        <div
+          className='
+            pt-24
+            grid
+            grid-cols-1
+            sm:grid-cols-2
+            md:grid-cols-3
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          '
+        >
+          <div>listings</div>
+        </div>
+      </Container>
+    </ClientOnly>
+  );
+};
+
+export default Home;
