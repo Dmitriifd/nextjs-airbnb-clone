bbc6efb002740a6055c14dc802fe9a1fa4f5bafc
Dmitriifd <idimka9@gmail.com>
2023-11-18 18:45:39 +0500

    NotFoundPage

---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+import EmptyState from '@/app/components/EmptyState';
+
+const NotFoundPage = () => {
+  return <EmptyState title='404 - Page Not Found' subtitle='Something went wrong!' />;
+};
+
+export default NotFoundPage;
